816514676222bc89f0a95b46df8e02b51898e6d8
kfirc <kfircohenyahel@gmail.com>
2024-12-19 17:44:51 +0000

    change the scale to seconds in the x axis

--- dataset/test/test_inpainting_dataset.py
+++ dataset/test/test_inpainting_dataset.py
@@ -3,6 +3,7 @@ from omegaconf import DictConfig
 import matplotlib.pyplot as plt
 import torch
 import torchaudio
+import numpy as np
 from dataset.audio_dataset_inpainting import AudioInpaintingConfig, AudioInpaintingDataset
 
 
@@ -14,6 +15,14 @@ def plot_spectrograms(clean_audio: torch.Tensor, masked_audio: torch.Tensor, mas
     n_fft = 512
     hop_length = 256
 
+    # Calculate time axis (in seconds)
+    audio_len_samples = clean_audio.shape[1]
+    duration = audio_len_samples / 16000  # assuming 16kHz sample rate
+
+    # For spectrograms
+    spec_time = np.linspace(0, duration, num=int(np.ceil(audio_len_samples / hop_length)))
+    spec_freq = np.linspace(0, 8000, n_fft // 2 + 1)  # Frequency axis up to Nyquist (8kHz)
+
     # Plot clean spectrogram
     spec_clean = torchaudio.transforms.Spectrogram(
         n_fft=n_fft,
@@ -21,9 +30,10 @@ def plot_spectrograms(clean_audio: torch.Tensor, masked_audio: torch.Tensor, mas
         power=2
     )(clean_audio)
     axes[0].imshow(torch.log10(spec_clean.squeeze() + 1e-8),
-                   aspect='auto', origin='lower')
+                   aspect='auto', origin='lower',
+                   extent=[0, duration, 0, 8000])
     axes[0].set_title('Clean Audio Spectrogram')
-    axes[0].set_ylabel('Frequency Bin')
+    axes[0].set_ylabel('Frequency (Hz)')
 
     # Plot masked spectrogram
     spec_masked = torchaudio.transforms.Spectrogram(
@@ -32,16 +42,19 @@ def plot_spectrograms(clean_audio: torch.Tensor, masked_audio: torch.Tensor, mas
         power=2
     )(masked_audio)
     axes[1].imshow(torch.log10(spec_masked.squeeze() + 1e-8),
-                   aspect='auto', origin='lower')
+                   aspect='auto', origin='lower',
+                   extent=[0, duration, 0, 8000])
     axes[1].set_title('Masked Audio Spectrogram')
-    axes[1].set_ylabel('Frequency Bin')
+    axes[1].set_ylabel('Frequency (Hz)')
 
     # Plot mask
-    axes[2].plot(mask.squeeze().numpy())
+    time_axis = np.linspace(0, duration, mask.shape[1])
+    axes[2].plot(time_axis, mask.squeeze().numpy())
     axes[2].set_title('Mask')
-    axes[2].set_xlabel('Time')
+    axes[2].set_xlabel('Time (seconds)')
     axes[2].set_ylabel('Amplitude')
     axes[2].set_ylim(-0.1, 1.1)
+    axes[2].grid(True)
 
     plt.tight_layout()
     plt.show()
